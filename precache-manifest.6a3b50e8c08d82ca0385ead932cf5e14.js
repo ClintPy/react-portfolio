86fdef0bcc72e8fc5d973ba4b69ba5cc952bf2a6
ClintPy <cmaruti93@gmail.com>
2019-10-19 13:20:52 +0300

    Updates

---
+++ precache-manifest.6a3b50e8c08d82ca0385ead932cf5e14.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a230230f75a0e18d9b5035868e1351",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "7fa03a55d0eaa1c8f81e",
+    "url": "/react-portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "dfcda88a5bac94aa1b34",
+    "url": "/react-portfolio/static/css/main.8f2bbedb.chunk.css"
+  },
+  {
+    "revision": "7fa03a55d0eaa1c8f81e",
+    "url": "/react-portfolio/static/js/2.76eae3cd.chunk.js"
+  },
+  {
+    "revision": "dfcda88a5bac94aa1b34",
+    "url": "/react-portfolio/static/js/main.6010f935.chunk.js"
+  },
+  {
+    "revision": "39bf8ceac68611279f87",
+    "url": "/react-portfolio/static/js/runtime~main.55e43ce6.js"
+  },
+  {
+    "revision": "7a0741cfeda9d5943be726db129f4f83",
+    "url": "/react-portfolio/static/media/api.7a0741cf.png"
+  },
+  {
+    "revision": "6c9f8b22f3ab0051cabbcf534999935a",
+    "url": "/react-portfolio/static/media/photo3.6c9f8b22.png"
+  },
+  {
+    "revision": "d47bf250260539ce078ae9d956ec230d",
+    "url": "/react-portfolio/static/media/photo4.d47bf250.png"
+  },
+  {
+    "revision": "0998f5697ff09750154cb6acc17f7e92",
+    "url": "/react-portfolio/static/media/prj2.0998f569.png"
+  },
+  {
+    "revision": "fd4fe15518aefec4798f1c0fc9063ac6",
+    "url": "/react-portfolio/static/media/prj3.fd4fe155.png"
+  },
+  {
+    "revision": "b902133571e274c7f6305bb37fa4ec0a",
+    "url": "/react-portfolio/static/media/prj4.b9021335.png"
+  },
+  {
+    "revision": "354f5343dd657cc3b6e91143fd1b83c1",
+    "url": "/react-portfolio/static/media/prj5.354f5343.png"
+  },
+  {
+    "revision": "01d24a1889f38ed16cc0937905e45df6",
+    "url": "/react-portfolio/static/media/prj6.01d24a18.png"
+  },
+  {
+    "revision": "6d573cf59ab4e6ad1ee0e84b139fc351",
+    "url": "/react-portfolio/static/media/prj7.6d573cf5.png"
+  },
+  {
+    "revision": "381c130117fe5cfa8b73c0f0345d9274",
+    "url": "/react-portfolio/static/media/prj8.381c1301.jpg"
+  }
+]);
